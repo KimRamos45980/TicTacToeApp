58d06ecb97b436647c0cd920effc301e5c34e232
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-10-22 12:04:46 -0700

    Add NewGAme OnClick event to clear all buttons text

--- app/src/main/java/com/example/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.tictactoeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttons = arrayOf(
+            findViewById(R.id.top_left_button),
+            findViewById(R.id.top_center_button),
+            findViewById(R.id.top_right_button),
+            findViewById(R.id.middle_left_button),
+            findViewById(R.id.middle_center_button),
+            findViewById(R.id.middle_right_button),
+            findViewById(R.id.bottom_left_button),
+            findViewById(R.id.bottom_center_button),
+            findViewById<Button>(R.id.bottom_right_button)
+        )
+
+        val newGame = findViewById<Button>(R.id.new_game)
+        newGame.setOnClickListener {
+            for (button in buttons) {
+                button.text = ""
+            }
+        }
     }
 }
