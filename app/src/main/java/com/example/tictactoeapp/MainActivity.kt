fcafe487552db2ab0ad67e91297303ced6945d18
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-10-26 13:27:33 -0700

    Assigned all buttons to their own variable

--- app/src/main/java/com/example/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.kt
@@ -1,28 +1,50 @@
 package com.example.tictactoeapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Grab the player turn text box view
+        val playerTurn = findViewById<TextView>(R.id.textView)
+        // Set StartupText to display a player letter and not just placeholder text
+        playerTurn.text = getString(R.string.TextView, "X")
+        // Holds which player letter's turn it is
+        var player = "X"
+
+        // All buttons on screen assigned to a variable
+        val topLeftButton = findViewById<Button>(R.id.top_left_button)
+        val topCenterButton = findViewById<Button>(R.id.top_center_button)
+        val topRightButton = findViewById<Button>(R.id.top_right_button)
+        val middleLeftButton = findViewById<Button>(R.id.middle_left_button)
+        val middleCenterButton = findViewById<Button>(R.id.middle_center_button)
+        val middleRightButton = findViewById<Button>(R.id.middle_right_button)
+        val bottomLeftButton = findViewById<Button>(R.id.bottom_left_button)
+        val bottomCenterButton = findViewById<Button>(R.id.bottom_center_button)
+        val bottomRightButton = findViewById<Button>(R.id.bottom_right_button)
+        val newGame = findViewById<Button>(R.id.new_game)
+
+        // Array of game buttons to make resetting easier
         val buttons = arrayOf(
-            findViewById(R.id.top_left_button),
-            findViewById(R.id.top_center_button),
-            findViewById(R.id.top_right_button),
-            findViewById(R.id.middle_left_button),
-            findViewById(R.id.middle_center_button),
-            findViewById(R.id.middle_right_button),
-            findViewById(R.id.bottom_left_button),
-            findViewById(R.id.bottom_center_button),
-            findViewById<Button>(R.id.bottom_right_button)
+            topLeftButton,
+            topCenterButton,
+            topRightButton,
+            middleLeftButton,
+            middleCenterButton,
+            middleRightButton,
+            bottomLeftButton,
+            bottomCenterButton,
+            bottomRightButton
         )
 
-        val newGame = findViewById<Button>(R.id.new_game)
+        // New game button reset
         newGame.setOnClickListener {
+            playerTurn.text = getString(R.string.TextView, "X")
+            player = "X"
             for (button in buttons) {
                 button.text = ""
             }
